4d799e7650dda2475abc7ba268d4d05c124016cd
Anas-Mhameed <mhameedanaskhader@gmail.com>
2024-05-05 10:43:26 +0300

    update worker.app

--- worker/worker.py
+++ worker/worker.py
@@ -2,25 +2,31 @@ from flask import Flask, request
 from pytube import YouTube
 from moviepy.editor import AudioFileClip
 from transformers import pipeline
+from random_word import RandomWords
 
 app = Flask(__name__)
 
+r = RandomWords()
 
 @app.route('/')
 def index():
     try:
-        youtube_url = request.args.get('youtube_url')
-        yt = YouTube(youtube_url)
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        filename = yt.title.replace(' ', '_') + '.mp4'
-        audio_stream.download(filename=filename)
+        # youtube_url = request.args.get('youtube_url')
+        # yt = YouTube(youtube_url)
+        # audio_stream = yt.streams.filter(only_audio=True).first()
+        # filename = yt.title.replace(' ', '_') + '.mp4'
+        # audio_stream.download(filename=filename)
 
-        audio = AudioFileClip(filename)
-        audio.write_audiofile(filename + '.flac', codec='flac')
+        # audio = AudioFileClip(filename)
+        # audio.write_audiofile(filename + '.flac', codec='flac')
 
-        filename = filename + '.flac'
-        result = generator(filename)
-        return result['text'], 200
+        # filename = filename + '.flac'
+        # result = generator(filename)
+        str = ""
+        for i in range(100):
+            str += r.get_random_word()
+            str += " "
+        return str, 200
     except Exception as e:
         return (str(e)), 500
 
